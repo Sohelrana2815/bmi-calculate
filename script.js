b99a819994262b04884084f90b094eafb5ce7328
Sohelrana2815 <sohel152302@gmail.com>
2024-05-26 10:14:13 +0600

    form

--- script.js
+++ script.js
@@ -34,7 +34,8 @@ function handleInch() {
   let inch = document.getElementById("inch").value;
   let calculate = inch * 0.0254;
   document.getElementById("inch-meter").innerHTML = "Your height in meter is:";
-  document.getElementById("calculate").innerHTML = calculate.toFixed(2)
+  document.getElementById("calculate").innerHTML = calculate.toFixed(2);
+  document.getElementById("meter").innerHTML = "  Meter";
 }
 
 function handleReload() {
